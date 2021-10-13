200fdd71409660ef5ebdecdef5d69fe97796c8c8
Sami Tondl <88879651+Schaaaami@users.noreply.github.com>
2021-10-13 19:27:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function showTime() {
 
 	minute = addZero(minute);
 	second = addZero(second);
-
+	let helo = api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={cb7fa35f68faa49bba4f60066f0ac487}
 	document.getElementById(
 		"date"
-	).innerHTML = `${today},<br> ${hour}:${minute}:${second}, <br> ${day}/${month}/${year}`;
+	).innerHTML = `${helo}, ${today},<br> ${hour}:${minute}:${second}, <br> ${day}/${month}/${year}`;
 	setTimeout(showTime, 0);
 }
 
