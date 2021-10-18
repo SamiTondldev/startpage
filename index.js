89c201d35a19cefd3f2de8551ac1a65712d346da
Sami Tondl <88879651+Schaaaami@users.noreply.github.com>
2021-10-18 15:29:11 +0200

    ^yep

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function showTime() {
 	let helo = api.openweathermap.org/data/2.5/weather?zip=14656,de&appid={cb7fa35f68faa49bba4f60066f0ac487}
 	document.getElementById(
 		"date"
-	).innerHTML = `${helo} <br> ${today},<br> ${hour}:${minute}:${second}, <br> ${day}/${month}/${year}`;
+	).innerHTML = `${today},<br> ${hour}:${minute}:${second}, <br> ${day}/${month}/${year}`;
 	setTimeout(showTime, 0);
 }
 
